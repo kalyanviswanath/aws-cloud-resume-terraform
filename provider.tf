8e67b834d4b58fdc703852846abb73c9ca463d0f
Kalyan <kalyanviswanath@hotmail.com>
2023-06-30 11:46:46 -0400

    more fix

--- provider.tf
+++ provider.tf
@@ -1,17 +1,11 @@
 terraform {
-/*
+
  cloud {
     organization = "watchdog"
 
     workspaces {
       name = "aws-cloud-resume-terraform"
     }
-  }*/
-  backend "s3" {
-    bucket = "github-aws-cloud-resume-terraform" 
-    region = "us-east-1"                   
-    key    = "global/s3/terraform.tfstate"
-    encrypt = true
   }
 
   required_providers {
