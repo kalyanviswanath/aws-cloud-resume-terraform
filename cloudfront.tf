e938dd85d5b612c4ee27f1f73dd6f0eefe10fde6
kalyanviswanath <42808734+kalyanviswanath@users.noreply.github.com>
2025-02-08 11:37:15 -0500

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -43,7 +43,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   default_cache_behavior {
     target_origin_id        = local.s3_origin_id
     viewer_protocol_policy  = "redirect-to-https"
-    cache_policy_id         = "658327ea-f89d-4fab-a63d-7e88639e58f6" # AWS Managed CachingDisabled
+    cache_policy_id         = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # AWS Managed CachingDisabled
     origin_request_policy_id = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf" # AWS Managed AllViewer
     allowed_methods         = ["GET", "HEAD"]
     cached_methods          = ["GET", "HEAD"]
