96252afd589df59f79d994559841c35cb6906838
kalyanviswanath <42808734+kalyanviswanath@users.noreply.github.com>
2025-08-31 19:23:07 -0400

    vistor count function fix

--- lambda.tf
+++ lambda.tf
@@ -75,7 +75,10 @@ resource "aws_lambda_function_url" "url1" {
 
     cors {
     allow_credentials = true
-    allow_origins     = ["*"]
+    allow_origins     = [
+      "https://resume.kalyanviswanath.com",
+      "https://kalyanviswanath.com"
+    ]
     allow_methods     = ["*"]
     allow_headers     = ["date", "keep-alive"]
     expose_headers    = ["keep-alive", "date"]
